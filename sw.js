f9c838407860ca8185d4aa9ce766156a09a94641
OIgold-web <mastad1432@gmail.com>
2026-02-07 09:39:57 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,21 @@
+const CACHE_NAME = "pwa-cache-v1"
+
 self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+          "index.html",
+          "style.css",
+          "main.js"
+        ])
+    })
+  )
   console.log("service workers installed")
+})
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(even.request)
+    })
+  )
 })
